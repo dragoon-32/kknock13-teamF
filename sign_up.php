d520ac1f3df9851ca2df56ac81533ee275756e39
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-07-26 03:16:39 +0900

    Add files via upload

---
+++ sign_up.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<form action="sign_act" method="post">
+ID: <input type="text" name="ID"><br>
+PASSWORD: <input type="text" name="PASSWORD"><br>
+NAME: <input type="text" name="NAME"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
