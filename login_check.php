42ed4369aa86bbb132623fbed2e308cf9d2c0e84
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-07-26 14:34:42 +0900

    Add files via upload

--- login_check.php
+++ login_check.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "ksh", "qwerasdf1", "test");
+$conn = mysqli_connect("localhost", "ksh", "qwerasdf1", "13FDB");
 //아이디 비교와 비밀번호 비교가 필요한 시점이다.
 // 1차로 DB에서 비밀번호를 가져온다 
 // 평문의 비밀번호와 암호화된 비밀번호를 비교해서 검증한다.
@@ -12,14 +12,11 @@ $result = mysqli_query($conn, $sql);
 
 $row = mysqli_fetch_array($result);
 $hashedPassword = $row['PW'];
-$row['id'];
-echo $row['id'];
 
 // echo $row['id'];
 // DB 정보를 가져왔으니 
 // 비밀번호 검증 로직을 실행하면 된다.
-$passwordResult = password_verify($password, $hashedPassword);
-if ($passwordResult) {
+if (password_verify($password, $hashedPassword)) {
     // 로그인 성공
     // 세션에 id 저장
     session_start();
