b1a8920782ac4cb9062f2dd450427c25ef287822
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-07-26 12:22:15 +0900

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="login_check.php" method="post">
+ID: <input type="text" name="ID"><br>
+PASSWORD: <input type="text" name="PASSWORD"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
