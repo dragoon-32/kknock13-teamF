b821bad8af37d171483cbc913c644f1866527094
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-08-23 22:50:58 +0900

    Add files via upload

--- write_action.php
+++ write_action.php
@@ -12,7 +12,7 @@ $query = "INSERT INTO board_info (title, text, date, hit, user)
         values('$title', '$content', '$date', 1, '$id')";
 
 
-$result = $connect->query($query);
+$result = mysqli_query($connect, $query);
 if ($result) {
 ?> <script>
         alert("<?php echo "게시글이 등록되었습니다." ?>");
