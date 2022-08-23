d7e296cd917abbbd433a298704953cd4341578d3
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-08-23 20:50:32 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        if(isset($_SESSION['userid'])){
+        ?>
+            <meta http-equiv="refresh" content="1;URL='main_board.php'">
+        <?php
+        } else {
+            ?>
+            <button onclick="location.href='login.php'">로그인</button><br/>
+            <button onclick="location.href='sign_up.php'">회원가입</button>
+        <?php
+        }
+        ?>
+    </body>
+</html>
