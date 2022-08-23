865cb84f8016adfe792a9d7de5e8dcb25a8b9367
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-08-23 22:46:04 +0900

    Add files via upload

---
+++ write_action.php
@@ -0,0 +1,27 @@
+<?php
+$connect = mysqli_connect("localhost", "ksh", "qwerasdf1", "13FDB") or die("fail");
+
+$id = $_POST['name'];                   //Writer
+$title = $_POST['title'];               //Title
+$content = $_POST['content'];           //Content
+$date = date('Y-m-d H:i:s');            //Date
+
+
+
+$query = "INSERT INTO board_info (title, text, date, hit, user) 
+        values('$title', '$content', '$date', 1, '$id')";
+
+
+$result = $connect->query($query);
+if ($result) {
+?> <script>
+        alert("<?php echo "게시글이 등록되었습니다." ?>");
+        location.href = "main_board.php";
+    </script>
+<?php
+} else {
+    echo "게시글 등록에 실패하였습니다.";
+}
+
+mysqli_close($connect);
+?>
