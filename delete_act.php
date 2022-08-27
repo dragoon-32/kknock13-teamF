0e9747f749cf5670bf23da3bb04ffe1672e14762
dragoon-32 <70985582+dragoon-32@users.noreply.github.com>
2022-08-27 11:03:57 +0900

    Add files via upload

--- delete_act.php
+++ delete_act.php
@@ -2,12 +2,13 @@
 $conn = mysqli_connect("localhost", "ksh", "qwerasdf1", "13FDB");
 $key = $_POST['key'];
 $num = $_POST['num'];
-$query = "delete from board_info where seq = '$num'";
+$query = "delete from board_info where seq = $num";
 if($key =='ksh91giro'){
     if(mysqli_query($conn, $query)){
         ?>
         <script>
         location.replace("main_board.php");
+        </script>
     <?php
     }
     else{
@@ -19,6 +20,7 @@ else{
     ?>
     <script>
         location.replace("main_board.php");
+    </script>
     <?php
 }
 mysqli_close($connect);
